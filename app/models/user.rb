5ff579eb1c5c0942db292ea017993dcae7397e9e
Sai Lakshmi Pattabiraman <sailakshmi.pattabiraman@gmail.com>
2019-03-03 13:03:53 -0800

    Write the methods need for Auth in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,22 @@ class User < ApplicationRecord
 
     before_validation :ensure_session_token
 
-    
+    def self.find_by_credentials
+    end
+
+    def self.generate_session_token
+        
+    end
+
+    def reset_session_token!
+        
+    end
+
+    def ensure_session_token
+        
+    end
+
+    def password=(password)
+        
+    end
 end
