8de2302541c4433abdec26129347e20aadcacd15
Sai Lakshmi Pattabiraman <sailakshmi.pattabiraman@gmail.com>
2019-03-03 17:25:44 -0800

    Implement the methods in the user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,22 +5,30 @@ class User < ApplicationRecord
 
     before_validation :ensure_session_token
 
-    def self.find_by_credentials
+    attr_reader :password
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        return user if user && BCrypt::Password.new(user.password_digest).is_password?(password)
+        nil
     end
 
     def self.generate_session_token
-        
+        SecureRandon::urlsafe_base64(16)
     end
 
     def reset_session_token!
-        
+        self.session_token = User.generate_session_token
+        self.save!
+        self.session_token
     end
 
     def ensure_session_token
-        
+        self.session_token ||= User.generate_session_token
     end
 
     def password=(password)
-        
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
     end
 end
