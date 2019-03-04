489bb28ed17cd55ac540e667ea211814cc8d85cf
Sai Lakshmi Pattabiraman <sailakshmi.pattabiraman@gmail.com>
2019-03-03 17:40:22 -0800

    Test the model and create Users and Sessions controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
     validates :username, :session_token, presence: true, uniqueness: true
     validates :password, length: { minimum: 6, allow_nil: true}
-    validates :password_digest, presence: { 'Password can\'t  be blank'}
+    validates :password_digest, presence: { message: 'Password can\'t  be blank'}
 
-    before_validation :ensure_session_token
+    after_initialize :ensure_session_token
 
     attr_reader :password
 
@@ -14,7 +14,7 @@ class User < ApplicationRecord
     end
 
     def self.generate_session_token
-        SecureRandon::urlsafe_base64(16)
+        SecureRandom::urlsafe_base64(16)
     end
 
     def reset_session_token!
